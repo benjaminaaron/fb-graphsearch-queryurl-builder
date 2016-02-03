ff8ad0f6e2a335be8667b550db3653fa7445ae6f
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-02-03 21:23:43 +0100

    prepare Group

---
+++ js/options/Group.js
@@ -0,0 +1,14 @@
+//People in [Group]
+//www.facebook.com/search/[group-id]/members
+
+var Group = function(btn){
+        
+};
+
+Group.prototype = {
+    __proto__: Template.prototype,
+    
+    getContrib: function(){
+
+    }
+};
